f964ebc3a7ea0d35e51390e9a0c3523d0467b90e
Nikita Baranov <nik.nik794@gmail.com>
2021-11-25 00:18:57 +0300

    Fix security

--- lab-04-spring/LabScheme/src/main/java/by/labscheme/labscheme/SecurityConfig.java
+++ lab-04-spring/LabScheme/src/main/java/by/labscheme/labscheme/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/secret").hasRole("ADMIN")
+                .antMatchers("/add").hasRole("ADMIN")
                 .and()
                 .formLogin()
                 .permitAll()
